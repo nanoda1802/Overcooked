1b5b0a7296594e3e00716aae0c6ed231ce0a8b65
Nanoda <bb201802@naver.com>
2025-11-12 18:26:43 +0900

    [feat] 유니티 버전 변경
    - 프리펩 수정
    - 플레이어 이동 테스트

---
+++ Assets/Script/Test.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Test : MonoBehaviour
+{
+    public Rigidbody rigid;
+
+    public float moveSpeed = 5f;
+    private Vector3 moveDir;
+    public float rotSpeed = 3f;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        var input = ctx.ReadValue<Vector2>();
+        moveDir.x = input.x;
+        moveDir.z = input.y;
+        rigid.velocity = moveSpeed * moveDir;
+        
+        if (moveDir.sqrMagnitude > 0.001f)
+        {
+            Quaternion targetRot = Quaternion.LookRotation(moveDir);
+            Quaternion smoothRot = Quaternion.Slerp(
+                rigid.rotation,
+                targetRot,
+                rotSpeed
+            );
+            rigid.MoveRotation(smoothRot);
+        }
+    }
+}
