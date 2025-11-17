d2f5c028f5e3b93344813cc326cc019183280f63
songwongbin <bb201802@naver.com>
2025-11-18 01:08:09 +0900

    Feat : 물체 들기&내려놓기
    - 해결중 : 물체를 든 상태에서 Dash 정상작동하지 않는 상황 (아마 든 물체의 mass 관련으로 예상중...)

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,58 +1,150 @@
-using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using SF = UnityEngine.SerializeField;
 
 // 참고 링크 https://rito15.github.io/posts/unity-rigidbody-move-and-jump/
+
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody rb;
-    private Vector3 moveDir;
-    
-    [SF, Range(0f, 100f)] private float moveSpeed = 5f;
-    [SF, Range(0f, 200f)] private float rotSpeed = 100f;
-    [SF, Range(0f, 100f)] private float dashForce = 5f;
-    [SF, Range(1f, 2f)] private float dashSpeedModifier = 1f;
-    
+    #region 필드와 프로퍼티
+    /* 컴포넌트 */
+    private Rigidbody _rb;
+    /* 이동 */
+    [Header("Move")]
+    [SF, Range(0f, 10f)] private float moveSpeed = 5f;
+    [SF, Range(0f, 1f)] private float rotRatio = 0.4f;
+    [SF, Range(0f, 10f)] private float dashForce = 5f;
+    [SF] private float dashSpeedModifier = 1f;
+    private Vector3 _moveDir;
+    /* 물체 잡기 놓기 */
+    [Header("Grab Item")]
+    [SF] private Transform pivot;
+    [SF, Range(60f, 180f)] private float fanAngle = 120f;
+    [SF, Range(1, 10)] private int rayCount = 7;
+    [SF, Range(0f, 5f)] private float rayDist = 1.5f;
+    [SF, Range(0f, 1f)] private float rayOffsetY = 0.1f;
+    [SF] private LayerMask targetLayer = 1<<7;
+    private Ray _detectRay;
+    private readonly HashSet<Transform> _detectedItems = new();
+    private Rigidbody _itemRb;
+    #endregion
+
+    #region 유니티 이벤트 메서드
     private void Awake()
     {
-        if (!TryGetComponent(out rb)) rb = gameObject.AddComponent<Rigidbody>();
-        rb.freezeRotation = true;
+        if (!TryGetComponent(out _rb))
+        {
+            _rb = gameObject.AddComponent<Rigidbody>();
+            _rb.freezeRotation = true;
+        }
+        
         Application.targetFrameRate = 60;
     }
 
     private void FixedUpdate()
     {
-        if (moveDir.sqrMagnitude <= 0.001f) return;
+        Debug.Log($"현재 속도 {_rb.velocity.sqrMagnitude:F1}");
+        if (_moveDir.sqrMagnitude <= 0.001f) return;
         Move();
         Rotate();
     }
+    #endregion
 
-    private void Move()
-    {
-        Vector3 moveOffset = moveSpeed * dashSpeedModifier * Time.fixedDeltaTime * moveDir;
-        rb.MovePosition(rb.position + moveOffset);
-    }
-
-    private void Rotate()
-    {
-        Quaternion smoothRot = Quaternion.Slerp(rb.rotation, Quaternion.LookRotation(moveDir), rotSpeed * Time.fixedDeltaTime);
-        rb.MoveRotation(smoothRot);
-    }
-
+    #region 인풋 이벤트 메서드
     public void OnMove(InputAction.CallbackContext ctx)
     {
         Vector2 input = ctx.ReadValue<Vector2>();
-        moveDir.x = input.x;
-        moveDir.z = input.y;
+        _moveDir.x = input.x;
+        _moveDir.z = input.y;
     }
 
     public void OnDash(InputAction.CallbackContext ctx)
     {
-        // 알아보고 다시 짜기!
-        // 입력시 짧게 대시, 누르고있으면 달리기 상태, 떼면 해제?
-        if (ctx.started) rb.AddForce(dashForce * moveDir, ForceMode.VelocityChange);
+        if (ctx.started) _rb.AddForce(dashForce * _moveDir, ForceMode.VelocityChange);
         if (ctx.performed) dashSpeedModifier = 2f;
         if (ctx.canceled) dashSpeedModifier = 1f;
+    }
+    
+    public void OnGrab(InputAction.CallbackContext ctx)
+    {
+        if (!ctx.started) return;
+        if (pivot.childCount > 0)
+        {
+            Ungrab();
+            return;
+        }
+
+        Detect();
+        if (_detectedItems.Count > 0) Grab();
+    }
+    #endregion
+    
+    private void Move()
+    {
+        Vector3 moveOffset = (moveSpeed * dashSpeedModifier * Time.fixedDeltaTime) * _moveDir;
+        _rb.MovePosition(_rb.position + moveOffset);
+    }
+
+    private void Rotate()
+    {
+        Quaternion smoothRot = Quaternion.Slerp(_rb.rotation, Quaternion.LookRotation(_moveDir), rotRatio);
+        _rb.MoveRotation(smoothRot);
+    }
+    
+    private void Detect()
+    {
+        _detectedItems.Clear();
+        
+        float angleStep = fanAngle / (rayCount - 1); // Ray 간 간격 (각도)
+        for (int i = 0; i < rayCount; i++)
+        {
+            float angle = -fanAngle / 2 + angleStep * i; // 이번 Ray의 Degree
+            _detectRay.origin = transform.position + rayOffsetY * Vector3.up;
+            _detectRay.direction = Quaternion.Euler(0, angle, 0) * transform.forward;
+
+            if (Physics.Raycast(_detectRay, out RaycastHit hit, rayDist, targetLayer))
+            {
+                _detectedItems.Add(hit.collider.transform);
+                Debug.DrawRay(_detectRay.origin, _detectRay.direction * hit.distance, Color.red);
+                Debug.Log("Hit: " + hit.collider.gameObject.name);
+            }
+            else
+            {
+                Debug.DrawRay(_detectRay.origin, _detectRay.direction * rayDist, Color.green);
+            }
+        }
+    }
+
+    private void Grab()
+    {
+        float minDist = float.MaxValue;
+        Transform closest = null;
+
+        foreach (Transform t in _detectedItems)
+        {
+            float dist = (t.position - transform.position).sqrMagnitude;
+            if (dist < minDist)
+            {
+                minDist = dist;
+                closest = t;
+            }
+        }
+
+        if (closest is not null && closest.gameObject.TryGetComponent(out _itemRb))
+        {
+            closest.SetParent(pivot);
+            closest.localPosition = Vector3.zero;
+            closest.localRotation = Quaternion.identity;
+            _itemRb.isKinematic = true;
+        }
+    }
+
+    private void Ungrab()
+    { 
+        Transform curItem = pivot.GetChild(0);
+        curItem.SetParent(null);
+        _itemRb.isKinematic = false;
+        _itemRb = null;
     }
 }
