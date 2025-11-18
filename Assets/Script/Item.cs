30851d257ca9fa30617e823a639d0646b4f8c617
songwongbin <bb201802@naver.com>
2025-11-18 23:41:29 +0900

    Feat : 물체 던지기
    - (구현O) Press 조작
    - (구현X) Hold 조작
    - (작업예정) 비행 중 물체 충돌 처리

--- Assets/Script/Item.cs
+++ Assets/Script/Item.cs
@@ -1,20 +1,54 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using SF = UnityEngine.SerializeField;
 
 public class Item : MonoBehaviour
 {
     public Rigidbody rb;
+    private TrailRenderer _trail;
     
     public bool isGrabbed = false;
-    public bool isThrown = false;
 
+    private bool _isThrown;
+    [SF] private Vector3 throwOrigin;
+    [SF] private Vector3 throwDir;
+    [SF,Range(0f, 1f)] private float throwDamp; // 0.4
+    [SF,Range(5f, 20f)] private float maxThrowDist; // 12
+    private float _throwForce = 10f;
+    
     private void Awake()
     {
         if (!TryGetComponent(out rb)) rb = gameObject.AddComponent<Rigidbody>();
+        _trail = GetComponent<TrailRenderer>();
     }
 
+    private void Start()
+    {
+        _trail.enabled = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_isThrown) Throwing();
+    }
+
+    public void SetThrowValues(Vector3 origin, Vector3 dir, float force)
+    {
+        _trail.enabled = _isThrown = true;
+        throwOrigin = origin;
+        throwDir = dir;
+        _throwForce =  force;
+    }
+
+    private void Throwing()
+    {
+        rb.velocity = _throwForce * throwDir;
+        float curDist = (throwOrigin - rb.position).sqrMagnitude;
+        if (curDist >= maxThrowDist)
+        {
+            rb.velocity *= throwDamp;
+            _trail.enabled = _isThrown = false;
+        }
+    }
     // 그냥 던지기 누르면 바라보는 방향으로 던지고, 꾹 누르면 던질 방향 설정할 수 있네
     // 플레이어가 달리면서 던지면 좀 더 멀리 간대
     // 대략 5칸 정도 날아가고 떨어지는 듯? 
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -10,33 +10,30 @@ using SF = UnityEngine.SerializeField;
 public class PlayerController : MonoBehaviour
 {
     #region 필드와 프로퍼티
-
     /* 컴포넌트 */
     private Rigidbody _rb;
-
     /* 이동 */
-    [Header("[ Move ]")] [SF, Range(0f, 10f)] private float moveSpeed = 5f;
+    [Header("[ Move ]")] 
+    [SF, Range(0f, 10f)] private float moveSpeed = 5f;
     [SF, Range(0f, 1f)] private float rotRatio = 0.4f;
     [SF, Range(0f, 10f)] private float dashForce = 5f;
     [SF] private float dashSpeedModifier = 1f;
-
     private Vector3 _moveDir;
-
     /* 물체 잡기 놓기 */
-    [Header("[ Grab Item ]")] [SF] private Transform pivot;
-    [SF, Range(0f, 5f)] private float detectOffset = 0.8f;
-    [SF] private Vector3 detectRange; // 0.7 0.7 0.4
-    [SF] private LayerMask targetLayer = 1 << 7;
+    [Header("[ Pick & Drop ]")] 
+    [SF] private Transform pivot;
+    [SF] private LayerMask targetLayer; // 1<<7
+    [SF] private Vector3 detectRange; // (0.8,0.7,0.4)
+    [SF, Range(0f, 5f)] private float detectOffset; // 0.8;
     private readonly Collider[] _detectedItems = new Collider[5];
-    private Item _grabbedItem;
-    private Collider _grabbedCol;
-    
-    [Header("[ Throw Item ]")]
-    [SF] private float throwForce = 10f;
+    private Item _pickedItem;
+    private Collider _pickedCol;
+    /* 물체 던지기 */
+    [Header("[ Throw ]")] 
+    [SF,Range(1f,20f)] private float throwForce; // 12f
     #endregion
 
     #region 유니티 이벤트 메서드
-
     private void Awake()
     {
         if (!TryGetComponent(out _rb))
@@ -63,11 +60,9 @@ public class PlayerController : MonoBehaviour
         Gizmos.DrawWireCube(Vector3.zero, detectRange * 2);
         Gizmos.matrix = Matrix4x4.identity; // 매트릭스를 리셋하여 다른 Gizmos에 영향 안 미치도록 함
     }
-
     #endregion
 
     #region 인풋 이벤트 메서드
-
     public void OnMove(InputAction.CallbackContext ctx)
     {
         Vector2 input = ctx.ReadValue<Vector2>();
@@ -82,35 +77,30 @@ public class PlayerController : MonoBehaviour
         if (ctx.canceled) dashSpeedModifier = 1f;
     }
 
-    public void OnGrab(InputAction.CallbackContext ctx)
+    public void OnPick(InputAction.CallbackContext ctx)
     {
         if (!ctx.started) return;
-
-        if (_grabbedItem is not null)
+        if (_pickedItem is not null)
         {
-            Ungrab();
+            Drop();
             return;
         }
-
-        if (Detect()) Grab();
+        if (Detect()) Pick();
     }
-
-
+    
     public void OnThrow(InputAction.CallbackContext ctx)
     {
-        if (_grabbedItem is null) return;
-        
+        if (_pickedItem is null) return;
         if (ctx.interaction is HoldInteraction)
         {
             // 홀드 -> 던질 방향 나오고 떼면 해당 방향으로 던짐
+            // 홀드 시간 조정해야함 input map 에서
+            // 홀드 시간 동안 방향 조절, 시간 초과시 마지막 방향으로 던짐
         }
         else if (ctx.interaction is PressInteraction)
         {
-            // 프레스 -> 현재 기준 전방으로 던짐
+            Throw(pivot.forward);
         }
-
-        Debug.Log("Throw");
-        Throw(transform.forward);
     }
     #endregion
 
@@ -135,11 +125,10 @@ public class PlayerController : MonoBehaviour
         return hits > 0;
     }
 
-    private void Grab()
+    private void Pick()
     {
         float minDist = float.MaxValue;
         GameObject closest = null;
-
         foreach (Collider col in _detectedItems)
         {
             if (col is null) continue;
@@ -148,22 +137,21 @@ public class PlayerController : MonoBehaviour
             {
                 minDist = dist;
                 closest = col.gameObject;
-                _grabbedCol = col;
+                _pickedCol = col;
             }
         }
-
-        if (closest is not null && closest.TryGetComponent(out _grabbedItem))
+        if (closest is not null && closest.TryGetComponent(out _pickedItem))
         {
-            AttachItem(_grabbedItem);
-            _grabbedCol.enabled = false;
+            AttachItem(_pickedItem);
+            _pickedCol.enabled = false;
         }
     }
 
-    private void Ungrab()
+    private void Drop()
     {
-        DetachItem(_grabbedItem);
-        _grabbedCol.enabled = true;
-        _grabbedItem = null;
+        DetachItem(_pickedItem);
+        _pickedCol.enabled = true;
+        _pickedItem = null;
     }
 
     private void AttachItem(Item item)
@@ -177,19 +165,15 @@ public class PlayerController : MonoBehaviour
     private void DetachItem(Item item)
     {
         item.transform.SetParent(null);
-        item.rb.isKinematic = _grabbedItem.isGrabbed = false;
+        item.rb.isKinematic = _pickedItem.isGrabbed = false;
     }
 
     private void Throw(Vector3 dir)
     {
-        // detach 하고
-        DetachItem(_grabbedItem);
-        // 현재 바라보는 방향으로 던져? item의 rb에 힘을 가하는? 달리다 던지면 좀 더 멀리
-        _grabbedItem.rb.AddForce(throwForce * dashSpeedModifier * dir, ForceMode.Impulse);
-        // 그리고 isThrown을 켜주고
-        _grabbedItem.isThrown = true;
+        DetachItem(_pickedItem);
+        _pickedItem.SetThrowValues(pivot.position, dir, throwForce * dashSpeedModifier);
         // 근데 바로 플레이어와 충돌해서 안 던져질 수 있음... 플레이어랑도 충돌할 거니까
-        _grabbedCol.enabled = true;
-        _grabbedItem = null;
+        _pickedCol.enabled = true;
+        _pickedItem = null;
     }
 }
