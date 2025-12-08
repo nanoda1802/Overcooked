01b0a7b898d23e088fa7eb0c593661a1c1e6c9bc
songwongbin <bb201802@naver.com>
2025-12-08 22:37:10 +0900

    Fix : PlayerData.cs 파일 분리
    - 하나의 cs 파일에 두 SO class를 두었더니 두 번째 녀석을 올바르게 load 하지 못 하기 때문에, MovementData와 DetectionData로 분리했다

--- Assets/Script/SO/PlayerDetectionData.cs
+++ Assets/Script/SO/PlayerDetectionData.cs
@@ -1,23 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using SF = UnityEngine.SerializeField;
-
-[CreateAssetMenu(fileName = "MovementData", menuName = "SO/Player/Movement")]
-public class PlayerMovementData : ScriptableObject
-{
-    [Header("[ Move & Rotate ]")]
-    [SF, Range(0f, 10f)] private float moveSpeed;
-    [SF, Range(0f, 1f)] private float rotRatio;
-    public float MoveSpeed => moveSpeed;
-    public float RotRatio => rotRatio;
-    
-    [Header("[ Dash & Run ]")]
-    [SF, Range(0f, 10f)] private float dashForce;
-    [SF, Range(0f, 5f)] private float runSpeedMultiplier;
-    public float DashForce => dashForce;
-    public float RunSpeedMultiplier => runSpeedMultiplier;
-}
 
 [CreateAssetMenu(fileName = "DetectionData", menuName = "SO/Player/Detection")]
 public class PlayerDetectionData : ScriptableObject
