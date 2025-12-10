94746cfce30f0481909452556ae6fc595a197761
Nanoda <bb201802@naver.com>
2025-12-10 19:44:55 +0900

    Revise : 스테이지 선택 씬 준비중

--- Assets/Script/TempPlayer.cs
+++ Assets/Script/TempPlayer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,15 +9,32 @@ using SF = UnityEngine.SerializeField;
 public class TempPlayer : MonoBehaviour
 {
     [SF] private NavMeshAgent agent;
-    
-    
-    
-    
+
+    public Vector3 cursorPos;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void OnCursorPos(InputAction.CallbackContext ctx)
+    {
+        cursorPos = ctx.ReadValue<Vector2>();
+    }
+
     public void OnLeftClick(InputAction.CallbackContext ctx)
     {
         
-        
-        
-        agent.SetDestination(transform.position);
+
+        // cursorPos = Input.mousePosition;
+        //
+        // Debug.Log($"OnLeftClick {cursorPos.x},{cursorPos.y}");
+        // if (cursorPos == Vector3.zero) return;
+        //
+        // Physics.Raycast(cursorPos, , out RaycastHit hit);
+        // Debug.Log($"Hit {hit.point.x},{hit.point.y},{hit.point.z}");
+        //
+        // Vector3 pos = Camera.main.ScreenToWorldPoint(hit.point);
+        // agent.SetDestination(pos);
     }
 }
